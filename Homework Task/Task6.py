27ee292ce45cc49db37d5045ace956f62355a9b3
Manoj Singh <singh.manoj209@gmail.com>
2024-08-20 02:00:21 +0530

    Task

---
+++ Homework Task/Task6.py
@@ -0,0 +1,7 @@
+### Task #6
+# - Develop a Python script that calculates the square and cube of a given number.
+num=int(input("Please enter the number"))
+square_of_number= num ** 2
+cube_of_number= num ** 3
+print(f"Square of number: {square_of_number}")
+print(f"Cube of number is: {cube_of_number}")
