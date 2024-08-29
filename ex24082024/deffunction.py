3f6ee05f8b48747a8b830c7e27b4f94a772f78fe
Manoj Singh <singh.manoj209@gmail.com>
2024-08-29 16:10:09 +0530

    200824

---
+++ ex24082024/deffunction.py
@@ -0,0 +1,9 @@
+num1=int(input("enter the number1"))
+num2=int(input("enter the number2"))
+num3=int(input("enter thr number3"))
+
+def sum_of_three_numbers(n1,n2,n3):
+    return n1+n2+n3
+# result= sum_of_three_numbers(num1,num2,num3)
+result= sum_of_three_numbers(n1=num1,n2=num2,n3=num3)
+print(result)
---
+++ ex24082024/deffunction1.py
@@ -0,0 +1,12 @@
+def sum_of_three_num(a=5,b=10,c=15):
+    return a+b+c
+result=sum_of_three_num(10,20,30)
+result1=sum_of_three_num(10,20)
+result2=sum_of_three_num(10)
+result3=sum_of_three_num()
+result4=sum_of_three_num(a=5,c=10)
+print(result)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
